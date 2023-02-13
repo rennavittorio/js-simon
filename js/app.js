a9c46d2318c3846eb6a25786bbf18344aead3804
rennavittorio <renna.vittorio@gmail.com>
2023-02-13 18:47:08 +0100

    ADD game algorithm completed (in console)

--- js/app.js
+++ js/app.js
@@ -7,3 +7,94 @@
 // aggiungete una interfaccia per il gioco:
 // almeno un pulsante play per poter ripetere il gioco al click sul pulsante
 // e almeno gestite la stampa del risultato in pagina
+
+//setup iniziale gioco
+let playBtnElement = document.querySelector('.play-btn');
+let counter;
+let clock = undefined;
+let time;
+
+//start game
+playBtnElement.addEventListener('click', function(){
+    
+    let rndNumList = generateRndIntegerNumList(5);
+    console.log(rndNumList);
+    
+    pcInput(rndNumList);
+    
+    counter = 5;
+    time = 1000;
+    clock = setInterval(userAnswer, time, rndNumList);
+
+    //valuto prompt
+
+})
+
+
+
+//////////////////////////////////////////////////
+//////////////// FUNCTION ////////////////////////
+//////////////////////////////////////////////////
+
+//genera lista num rnd da 0 a numItems
+function generateRndIntegerNumList (numItems){
+    let rndList = []
+    while (rndList.length < numItems){
+        let rndNum = Math.floor(Math.random()*10);
+        if (!rndList.includes(rndNum)){
+            rndList.push(rndNum);
+        }
+        
+    }
+    
+    return rndList;
+    
+}
+
+
+function pcInput (list){
+    for (let i = 0; i < list.length; i++){
+        alert(list[i]);
+    }
+
+}
+
+
+function userAnswer(rndNumList) {
+    counter--;
+    console.log(counter);
+
+    userInput = '';
+    if (counter === 0) {
+        //interrompere
+        clearInterval(clock);
+        let userInput = prompt('insert numbers');
+        userInputResult(userInput, rndNumList);
+
+    }
+
+}
+
+
+function userInputResult (userInput, rndNumList){
+    //ctrl
+    let userInputCtrlArray = [];
+    for (let i = 0; i < userInput.length; i++){
+        userInputCtrlArray.push(parseInt(userInput[i]));
+    }
+    
+    //points count
+    let pointsCounter = 0;
+    let rigthInputList = []
+    for (let i = 0; i < rndNumList.length; i++){
+        if (rndNumList.includes(userInputCtrlArray[i])){
+            pointsCounter++;
+            rigthInputList.push(userInputCtrlArray[i]);
+        }
+        
+    }
+    
+   console.log('user points', pointsCounter);
+   console.log('rigth inputs', rigthInputList);
+
+}
