3c72228c86f7c07083dfc9fd1867ac922bbdb13d
rennavittorio <renna.vittorio@gmail.com>
2023-02-13 15:07:13 +0100

    START file setup completed + STYLE

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// CONSEGNA
+// Visualizzare in pagina 5 numeri casuali con alert.
+// Dopo che l’utente ha chiuso l’alert fate partire un timer di 30 secondi.
+// Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
+// BONUS
+// aggiungete una interfaccia per il gioco:
+// almeno un pulsante play per poter ripetere il gioco al click sul pulsante
+// e almeno gestite la stampa del risultato in pagina
